a9c1fcc8d9ff5649187766d95ecc65931971b4ad
SergiuA17 <98698995+SergiuLA17@users.noreply.github.com>
2023-08-08 15:12:29 +0300

    test

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ docker push bloodysoon/cheatsheet:$NEW_VERSION
 # Stop and remove previous container
 docker stop cheatsheet
 docker rm cheatsheet
-./update-deployment.sh
+././update-workload.sh
 kubectl apply -f Kubernetes/service.yaml
-kubectl apply -f Kubernetes/workload.yaml
+kubectl apply -f Kubernetes/workload.yml
 
 # Run the new container
 docker run -d -p 4000:3000 --name cheatsheet bloodysoon/cheatsheet:$NEW_VERSION
--- update-workload.sh
+++ update-workload.sh
@@ -3,5 +3,20 @@
 # Get the version from package.json
 IMAGE_VERSION=$(jq -r '.version' package.json)
 
-# Update the deployment.yaml with the new version
-sed -i "s|bloodysoon/cheatsheet:v[0-9.]\+|bloodysoon/cheatsheet:v$IMAGE_VERSION|" deployment.yaml
+# Update the deployment.yaml with the new version using Python
+python3 - <<EOF
+import re
+
+new_version = "$IMAGE_VERSION"
+
+with open("Kubernetes/workload.yml", "r") as f:
+    content = f.read()
+
+pattern = r'(bloodysoon/cheatsheet:v)[0-9.]+'
+replacement = f'\\g<1>{new_version}'
+
+updated_content = re.sub(pattern, replacement, content)
+
+with open("Kubernetes/workload.yml", "w") as f:
+    f.write(updated_content)
+EOF

8c68aaa8bd418b40992f3c6eaf3a369d041c44ec
SergiuA17 <98698995+SergiuLA17@users.noreply.github.com>
2023-08-08 12:34:59 +0300

    script file changes

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-npm run minor
 
+# Bump the version using npm
+NEW_VERSION=$(npm version minor)
+
+# Build Docker image and tag with the new version
 docker build -t cheatsheet .
-docker tag cheatsheet cheatsheet:$NEW_VERSION
+docker tag cheatsheet bloodysoon/cheatsheet:$NEW_VERSION
 docker push bloodysoon/cheatsheet:$NEW_VERSION
 
+# Stop and remove previous container
 docker stop cheatsheet
 docker rm cheatsheet
 
-docker 
-
-docker run -d -p4000:3000 --name cheatsheet cheatsheet
+# Run the new container
+docker run -d -p 4000:3000 --name cheatsheet bloodysoon/cheatsheet:$NEW_VERSION
